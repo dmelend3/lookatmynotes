d1abc8d23896aa2a0f7547c826a5edf34b7376d6
dmelend3 <santos_9602@hotmail.com>
2020-02-23 14:23:26 -0600

    Added Tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,9 @@
 </template>
 
 <script>
-  export default {
-    name:"App"
-  }
+export default {
+  name: "App"
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
